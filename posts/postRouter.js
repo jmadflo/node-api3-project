96d8096193d0b7e39ae2aef8c8a18093a389c123
Juan Gerardo Madero Flores <juan@Juans-MacBook.attlocal.net>
2020-05-06 23:07:46 -0700

    validatePostId completed

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,27 +1,38 @@
-const express = require('express');
+const express = require('express')
+const postData = require('../posts/postDb')
 
-const router = express.Router();
+const router = express.Router()
+
+// These routes have a base url of http://localhost:7000/api/posts
 
 router.get('/', (req, res) => {
   // do your magic!
-});
+})
 
 router.get('/:id', (req, res) => {
   // do your magic!
-});
+})
 
 router.delete('/:id', (req, res) => {
   // do your magic!
-});
+})
 
 router.put('/:id', (req, res) => {
   // do your magic!
-});
+})
 
 // custom middleware
 
 function validatePostId(req, res, next) {
-  // do your magic!
+  // get post with the params id and assign it to req.post, otherwise return a 400 error
+  postData.getById(req.params.id)
+    .then(post => {
+      req.post = post
+      next()
+    })
+    .catch(() => {
+      res.status(400).json({ message: "invalid post id" })
+    })
 }
 
-module.exports = router;
+module.exports = router
